d15a52244cc7340c09124c4724dc445f715049d3
Srom Michael J <>
2019-04-11 13:30:22 -0700

    Added Joystick and player movement

--- RTPS/RTPS/GameScene.swift
+++ RTPS/RTPS/GameScene.swift
@@ -16,7 +16,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        let background = SKSpriteNode(imageNamed: "RTPS_Map_Background")
+       /* let background = SKSpriteNode(imageNamed: "RTPS_Map_Background")
         background.position = CGPoint(x: size.width / 2, y : size.height / 2)
         background.zRotation = 1.5708
         
@@ -25,12 +25,13 @@ class GameScene: SKScene {
         mainCharacter.setScale(0.18)
 
         let zombie = SKSpriteNode(imageNamed: "Zombie")
-        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        zombie.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        zombie.setScale(0.18)
         
         
         addChild(background)
         addChild(mainCharacter)
-        addChild(zombie)
+        addChild(zombie)*/
     }
     
     
